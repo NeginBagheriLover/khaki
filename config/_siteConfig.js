9cc41333ba2a5a6566c12c8709b7b15641cbbccc
NeginBagheriLover <72755341+NeginBagheriLover@users.noreply.github.com>
2020-12-05 16:30:20 +0330

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -30,11 +30,8 @@ export default {
       name: 'Categories',
       link: '/categories'
     },
-    {
-      name: 'Github Repo',
-      link: 'https://github.com/danielkellyio/awake-template',
-      target: '_blank'
-    },
+    
+
     {
       name: 'Awake+ Premium',
       link: 'https://awake-premium-lp.netlify.com/',
